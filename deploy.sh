fb9487f8cb904ed973b8fd348a4d633f9d5dd3a3
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-03-05 19:19:19 +0100

    update to new repo name

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,11 @@
 #!/usr/bin/env sh
 
-# Replace this later with github action workflow https://vitejs.dev/guide/static-deploy.html
-
-# abort on errors
-set -e
-
 # build
 npm run build
 
+# deploy
 git add dist -f
+
 git commit -m 'deploy'
-git subtree push --prefix dist origin:main gh-pages
 
-# # navigate into the build output directory
-# cd dist
-
-# # if you are deploying to a custom domain
-# # echo 'www.example.com' > CNAME
-
-# git init
-# git add -A
-# git commit -m 'deploy'
-
-# # if you are deploying to https://<USERNAME>.github.io
-# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:jtdLab/my_vue_app.git main:gh-pages
-
-# cd -
+git subtree push --prefix dist origin gh-pages
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/my_vue_app/',
+  base: '/vue_test/',
 })
